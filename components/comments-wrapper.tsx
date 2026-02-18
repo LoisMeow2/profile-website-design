846321a93789c18586b8802a5cab4e48b0dcb105
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-02-18 19:17:42 +0000

    library update

---
+++ components/comments-wrapper.tsx
@@ -0,0 +1,11 @@
+import CommentsWrapper from "../components/comments-wrapper";
+
+export default function HomePage() {
+  return (
+    <main>
+      <h1>My Profile</h1>
+      {/* Other content */}
+      <CommentsWrapper />
+    </main>
+  );
+}
