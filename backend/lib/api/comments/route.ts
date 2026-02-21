fb50424dcdfb1ccde0b5a52b97e97b899e3cb655
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-02-21 18:36:40 +0000

    ;

--- backend/lib/api/comments/route.ts
+++ backend/lib/api/comments/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from "next/server"
-import { supabase } from "@/backend/lib/supabaseClient"
+import { NextResponse } from "next/server";
+import { supabase } from "@/backend/lib/supabaseClient";
 
 export async function GET() {
   const { data, error } = await supabase
